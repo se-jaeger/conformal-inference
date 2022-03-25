f42b7746c927f222ecfb6982e155093bff30eb3f
Sebastian Jäger <git@sebastian-jaeger.me>
2022-03-25 14:55:22 +0100

    rename sklearn.py -> base.py

--- conformal_inference/models/base.py
+++ conformal_inference/models/base.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
-from abc import ABC, abstractmethod
+from abc import ABC
 from copy import deepcopy
 from logging import getLogger
-from typing import Any, Dict, Optional, Tuple, Union
+from typing import Any, Dict, Tuple, Union
 
 import numpy as np
 from numpy.typing import ArrayLike, NDArray
@@ -11,6 +11,8 @@ from sklearn.base import BaseEstimator, is_classifier, is_regressor
 from sklearn.model_selection import train_test_split
 from sklearn.utils import check_array
 from sklearn.utils.validation import check_is_fitted
+
+from .utils import calculate_q_hat, check_in_range
 
 logger = getLogger(__name__)
 
@@ -20,7 +22,7 @@ class InductiveConformalPredictor(ABC):
     Inductive Conformal Predictors are originally described in Section 4.1 of:
         Vovk, V., Gammerman, A., & Shafer, G. (2005). Algorithmic Learning in a Random World.
 
-    Other literature call this approach Split Conformal Predictors.
+    Other literature refer to this approach as Split Conformal Predictors.
     """
 
     calibration_nonconformity_scores_: Union[NDArray, Dict[Any, NDArray]]
@@ -35,19 +37,15 @@ class InductiveConformalPredictor(ABC):
         self._fit = fit
         self.predictor = deepcopy(predictor)
 
-    def check_in_range(self, number: float, name: str, range: Tuple[int, int] = (0, 1)) -> None:
-        if number < range[0] or number > range[1]:
-            raise ValueError(f"Variable '{name}' is not valid! Need to be: 0 <= {name} <= 1")
-
     def check_and_split_X_y(
         self, X: ArrayLike, y: ArrayLike, calibration_size: float
     ) -> Tuple[NDArray, NDArray, NDArray, NDArray]:
-        self.check_in_range(calibration_size, "calibration_size")
+        check_in_range(calibration_size, "calibration_size")
 
         X = check_array(np.array(X), force_all_finite="allow-nan", estimator=self.predictor)
         y = np.array(y).ravel()  # make sure target is 1d array
 
-        if self.fit:
+        if self._fit:
             X_training, X_calibration, y_training, y_calibration = train_test_split(
                 X, y, test_size=calibration_size
             )
@@ -63,34 +61,8 @@ class InductiveConformalPredictor(ABC):
             y_calibration,
         )
 
-    @staticmethod
-    def calculate_q_hat(nonconformity_scores: NDArray, confidence_level: float) -> float:
 
-        n = len(nonconformity_scores)
-        quantile = confidence_level * (1 + 1 / n)
-
-        # clip `quantile` to make sure it is in 0 <= quantile <= 1
-        quantile = 1 if quantile > 1 else 0 if quantile < 0 else quantile
-
-        return np.quantile(nonconformity_scores, quantile, method="higher")
-
-    @staticmethod
-    @abstractmethod
-    def _calculate_nonconformity_scores(y: Optional[NDArray], y_hat: NDArray) -> NDArray:
-        pass
-
-    @abstractmethod
-    def fit(
-        self, X: ArrayLike, y: ArrayLike, calibration_size: float = 0.2
-    ) -> InductiveConformalPredictor:
-        pass
-
-    @abstractmethod
-    def predict(self, X: ArrayLike, confidence_level: float = 0.9) -> NDArray:
-        pass
-
-
-class InductiveConformalClassifier(InductiveConformalPredictor):
+class ConformalClassifier(InductiveConformalPredictor):
     """
     TODO
     """
@@ -98,7 +70,7 @@ class InductiveConformalClassifier(InductiveConformalPredictor):
     calibration_nonconformity_scores_: Dict[Any, NDArray]
 
     @staticmethod
-    def _calculate_nonconformity_scores(y: Optional[NDArray], y_hat: NDArray) -> NDArray:
+    def _calculate_nonconformity_scores(y_hat: NDArray) -> NDArray:
         return 1 - y_hat
 
     def fit(
@@ -114,7 +86,7 @@ class InductiveConformalClassifier(InductiveConformalPredictor):
 
         self.label_2_index_ = {x: index for index, x in enumerate(self.predictor.classes_)}
         nonconformity_scores = self._calculate_nonconformity_scores(
-            y=y_calibration, y_hat=self.predictor.predict_proba(X_calibration)
+            y_hat=self.predictor.predict_proba(X_calibration)
         )
 
         if self._conditional:
@@ -131,7 +103,7 @@ class InductiveConformalClassifier(InductiveConformalPredictor):
         return self
 
     def predict(self, X: ArrayLike, confidence_level: float = 0.9) -> NDArray:
-        self.check_in_range(confidence_level, "confidence_level")
+        check_in_range(confidence_level, "confidence_level")
         check_is_fitted(
             self,
             attributes=["calibration_nonconformity_scores_", "label_2_index_"],
@@ -140,23 +112,21 @@ class InductiveConformalClassifier(InductiveConformalPredictor):
         X = check_array(X, force_all_finite="allow-nan", estimator=self.predictor)
 
         y_hat = self.predictor.predict_proba(X)
-        nonconformity_scores = self._calculate_nonconformity_scores(None, y_hat=y_hat)
+        nonconformity_scores = self._calculate_nonconformity_scores(y_hat=y_hat)
 
         # prepare prediction sets. If class is not included in prediction, use `np.nan`
         prediction_sets = np.empty(nonconformity_scores.shape)
         prediction_sets[:] = np.nan
 
         for label, index in self.label_2_index_.items():
-            q_hat = self.calculate_q_hat(
-                self.calibration_nonconformity_scores_[label], confidence_level
-            )
+            q_hat = calculate_q_hat(self.calibration_nonconformity_scores_[label], confidence_level)
 
             prediction_sets[nonconformity_scores[:, index] < q_hat, index] = label
 
         return prediction_sets
 
 
-class InductiveConformalRegressor(InductiveConformalPredictor):
+class ConformalRegressor(InductiveConformalPredictor):
     """
     Algorithm described in:
         Lei, J., G'Sell, M., Rinaldo, A., Tibshirani, R. J., & Wasserman, L. (2018).
@@ -167,7 +137,7 @@ class InductiveConformalRegressor(InductiveConformalPredictor):
     calibration_nonconformity_scores_: NDArray
 
     @staticmethod
-    def _calculate_nonconformity_scores(y: Optional[NDArray], y_hat: NDArray) -> NDArray:
+    def _calculate_nonconformity_scores(y: NDArray, y_hat: NDArray) -> NDArray:
         return np.abs(y - y_hat)
 
     def fit(
@@ -200,13 +170,13 @@ class InductiveConformalRegressor(InductiveConformalPredictor):
         return self
 
     def predict(self, X: ArrayLike, confidence_level: float = 0.9) -> NDArray:
-        self.check_in_range(confidence_level, "confidence_level")
+        check_in_range(confidence_level, "confidence_level")
         check_is_fitted(self, attributes=["calibration_nonconformity_scores_"])
 
         X = check_array(X, force_all_finite="allow-nan", estimator=self.predictor)
 
         y_hat = self.predictor.predict(X)
-        q_hat = self.calculate_q_hat(self.calibration_nonconformity_scores_, confidence_level)
+        q_hat = calculate_q_hat(self.calibration_nonconformity_scores_, confidence_level)
 
         if self._conditional:
             uncertainty_prediction = self.uncertainty_predictor.predict(X)
@@ -221,7 +191,7 @@ class InductiveConformalRegressor(InductiveConformalPredictor):
         return np.stack((y_hat_lower_bound, y_hat_upper_bound), axis=1)
 
 
-def InductiveConformalPredictorFactory(
+def ConformalPredictor(
     predictor: BaseEstimator,
     conditional: bool = True,
     fit: bool = True,
@@ -230,14 +200,14 @@ def InductiveConformalPredictorFactory(
         check_is_fitted(predictor)
 
     if is_classifier(predictor):
-        return InductiveConformalClassifier(
+        return ConformalClassifier(
             predictor=predictor,
             conditional=conditional,
             fit=fit,
         )
 
     elif is_regressor(predictor):
-        return InductiveConformalRegressor(
+        return ConformalRegressor(
             predictor=predictor,
             conditional=conditional,
             fit=fit,
